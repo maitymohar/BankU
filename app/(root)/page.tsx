406156418ebff99b3260cbd19d3e81cf502ee43d
maitymohar <maitymohar2005@gmail.com>
2024-05-13 12:05:57 +0530

    New Files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn = {firstname: 'Mohar'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+       <HeaderBox
+       type="greeting"
+       title="Welcome"
+       user={loggedIn ? .firstname || 'Guest'}
+       subtext="Access and manage your account and transactions successfully"
+       />
+       <TotalBalanceBox
+       accounts={[]}
+       totalBanks={1}
+       totalCurrentBalance={1250.34}
+       />
+       </header>
+      </div>
+    </section>
   )
 }
 
